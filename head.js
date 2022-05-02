d973fe65a794e0e135e1c542559e4a2b9f84f153
Heather <hmcleod29@gmail.com>
2022-05-02 17:13:36 -0400

    Implement head challenge done

---
+++ head.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual}  ===  ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function head(array){
+  return array[0];
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Lighthouse", "Labs", "Bootcamp"]), "Hello");
