13d9457cc62eb3275483cf624870cdd097bf0960
Heather <hmcleod29@gmail.com>
2022-05-05 18:42:45 -0400

    Map creates an array of the first letter of every word

---
+++ map.js
@@ -0,0 +1,38 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  let result = true;
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        result = false;
+      }
+    }
+  } else {
+    result = false;
+  }
+  return result;
+};
+
+
+
+const results1 = map(words, word => word[0]);
+
+console.log(assertArraysEqual(["g" , "c", "t", "m", "t"], results1));
+
