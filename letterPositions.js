85f07691821c4750956eb08865a1b34123b5e07f
Heather <hmcleod29@gmail.com>
2022-05-04 21:31:09 -0400

    return all the indices (zero-based positions) in the string where each character is found.

---
+++ letterPositions.js
@@ -0,0 +1,39 @@
+const eqArrays = function(array1, array2) {
+  let result = true;
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        result = false;
+      }
+    }
+  } else {
+    result = false;
+  }
+  return result;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    } else {
+      results[sentence[i]] = [i];
+    }
+  }
+  return results;
+};
+
+
+const sentence = ('hello');
+
+console.log(assertArraysEqual(letterPositions("hello").l, [1]));
