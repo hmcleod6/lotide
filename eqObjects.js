e3ba942002021547c0cd11b97ac6c9787ffe1139
Heather <hmcleod29@gmail.com>
2022-05-05 13:00:10 -0400

    Returns true if both objects have identical keys without identical values

---
+++ eqObjects.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual}  ===  ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+  if (Object.entries(object1).length !== Object.entries(object2).length)
+    return false;
+  for (let key of Object.keys(object1)) {
+    if (Array.isArray(object1[key]) !== Array.isArray(object2[key])) return false;
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) 
+      return false;
+    }
+    for (const [key, value] of Object.entries(object1)) {
+      if (typeof value === "object") {
+        if (!eqObjects(obj1[key], obj2[key])) 
+        return false;
+      } else {
+        if (object1[key] !== object2[key]) 
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "2" };
+assertEqual(eqObjects(ab, ba), true); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false); // => false
