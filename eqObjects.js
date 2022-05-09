512ed0509078dd8e5b3736849b09b03c8ffe2b7f
Heather <hmcleod29@gmail.com>
2022-05-09 19:17:25 -0400

    Updated code so it was easier to read and used eqArrays inside the function

--- eqObjects.js
+++ eqObjects.js
@@ -1,38 +1,27 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual}  ===  ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
 
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
   if (Object.entries(object1).length !== Object.entries(object2).length)
-    return false;
-  for (let key of Object.keys(object1)) {
-    if (Array.isArray(object1[key]) !== Array.isArray(object2[key])) return false;
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      if (!eqArrays(object1[key], object2[key])) 
-      return false;
-    }
-    for (const [key, value] of Object.entries(object1)) {
-      if (typeof value === "object") {
-        if (!eqObjects(object1[key], object2[key])) 
+    return false; // if the objects are not the same length, you know they are not going to be equal
+  for (let key in object1) { // loop through the keys in object 1
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) { //
+      if (eqArrays(object1[key], object2[key]) === false) { // using function eqArrays to determine whether the keys in Obj 1 and 2 are perfect matches, returns true or false
         return false;
-      } else {
-        if (object1[key] !== object2[key]) 
+      } else if (object1[key] !== object2[key]) {
         return false;
-      }
     }
-  }
+  };
   return true;
 };
-
+};
 const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "2" };
+const ba = { b: "2", a: "1" };
 assertEqual(eqObjects(ab, ba), true); // => true
 
 const abc = { a: "1", b: "2", c: "3" };
 assertEqual(eqObjects(ab, abc), false); // => false
+
+
+  module.exports = eqObjects;
