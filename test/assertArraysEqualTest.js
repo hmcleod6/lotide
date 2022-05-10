5f2c7b986ff327fbc382e1f9661a58fc3fb60313
Heather <hmcleod29@gmail.com>
2022-05-09 20:13:00 -0400

    Adding tests for assertArraysEqualTest.js

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,6 +1,10 @@
 const assertArraysEqual = require('../assertArraysEqual');
 const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
+const assert = require('chai').assert;
 
-
-assertArraysEqual([1, 4], [1, 3, 4]);
+describe("#assertArraysEqual", () => {
+  it("returns [1, 2, 3] for [1, 2, 3]", () => {
+    assert.strictEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+  });
+});
