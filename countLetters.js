c7a2bfdb851cd4e1762d6c7711047c8de39a7bb6
Heather <hmcleod29@gmail.com>
2022-05-09 20:45:39 -0400

    Refactored

--- countLetters.js
+++ countLetters.js
@@ -1,19 +1,12 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual}  ===  ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
+const assertEqual = require('./assertEqual');
 
 const countLetters = function(words) {
   const result = {};
   const newWordsString = words.split(" ").join("");
   for (const letter of newWordsString) {
-    if (result[letter]) { 
+    if (result[letter]) {
       result[letter] += 1;
-    } else { 
+    } else {
       result[letter] = 1;
     }
   }
@@ -21,3 +14,5 @@ const countLetters = function(words) {
 };
 
 console.log(countLetters("my name is heather"));
+
+module.exports = countLetters;
