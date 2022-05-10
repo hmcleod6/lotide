10a2232abac721c2a37fca03ece50a8896585784
Heather <hmcleod29@gmail.com>
2022-05-09 21:24:47 -0400

    Refactored and linted

--- letterPositions.js
+++ letterPositions.js
@@ -1,25 +1,6 @@
-const eqArrays = function(array1, array2) {
-  let result = true;
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        result = false;
-      }
-    }
-  } else {
-    result = false;
-  }
-  return result;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`❌❌❌Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
-
+const eqArrays = require('./eqArrays');
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
 
 const letterPositions = function(sentence) {
   const results = {};
@@ -34,8 +15,8 @@ const letterPositions = function(sentence) {
 };
 
 
-const sentence = ('hello');
+// const sentence = ('hello');
 
-console.log(assertArraysEqual(letterPositions("hello").l, [1]));
+// console.log(assertArraysEqual(letterPositions("hello").l, [1]));
 
 module.exports = letterPositions;
