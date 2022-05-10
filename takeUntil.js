ec7cf6fb5d6e614e6a8a3cf3d9f0a69b7fa986d9
Heather <hmcleod29@gmail.com>
2022-05-09 21:28:09 -0400

    Refactored and linted

--- takeUntil.js
+++ takeUntil.js
@@ -1,24 +1,6 @@
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`❌❌❌Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
-
-const eqArrays = function(array1, array2) {
-  let result = true;
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        result = false;
-      }
-    }
-  } else {
-    result = false;
-  }
-  return result;
-};
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 
 const takeUntil = function(array, callback) {
@@ -33,17 +15,17 @@ const takeUntil = function(array, callback) {
   return results;
 };
 
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
+// const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+// const results1 = takeUntil(data1, x => x < 0);
+// console.log(results1);
 
-const data3 = '---';
-const results3 = takeUntil(data3, x => x === ['-','-']);
-console.log(results3);
+// const data3 = '---';
+// const results3 = takeUntil(data3, x => x === ['-','-']);
+// console.log(results3);
 
 
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
+// const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+// const results2 = takeUntil(data2, x => x === ',');
+// console.log(results2);
 
 module.exports = takeUntil;
